b9db662e02bce385faa1330b1c34e9c95086cb7f
sahyunjin <tkguswls1106@naver.com>
2023-09-14 13:54:47 +0900

    첫 기초작업 커밋.

---
+++ src/main/java/com/smud/socksensespringproject/SocksenseSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.smud.socksensespringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksenseSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksenseSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smud/socksensespringproject/SocksenseSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smud.socksensespringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksenseSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
